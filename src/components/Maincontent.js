9bdb1de0cbe06ddbce8ad265e4a824e9608a73b0
muli reuben <mulireuben57@gmail.com>
2024-03-07 16:56:31 +0300

    commit

--- src/components/Maincontent.js
+++ src/components/Maincontent.js
@@ -1,9 +1,24 @@
+
+
+
+
 function Maincontent(){
+
+    let imgLink
+
+fetch("https://api.imgflip.com/get_memes") 
+.then(response=>response.json())
+.then(resObj=> imgLink = resObj.data.memes[0].url)
+
+    
+    
+
+
     return(
         <main>
     <h1 className="maincontent--title">THIS IS AN INTRODUCTION TO MY SIMPLE PAGE</h1>
-     
-         <ul className="main--move" >
+        { imgLink &&  <img src={imgLink} alt="" width="300px" height="300px" ></img> }
+             <ul className="main--move" >
             <li>am willing to learn </li>
             <li>am willing to enjoy </li>
             <li>am willing to be busy </li>
